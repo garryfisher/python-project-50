167b81683a8887d0c7df8b2db4e9e75ba8faec60
garryfisher <gwinogradow@gmail.com>
2022-12-23 21:12:59 +0300

    FIX gendiff

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -4,29 +4,27 @@ IN_FILE2 = "+ "
 IN_FILES = "  "
 
 
+
 def get_file(path_to_file):
-    with open(path_to_file) as my_file:
-        return my_file.read()
+    with open(path_to_file, 'r') as file:
+        return file.read()
 
 
-def generate_diff(first_file, second_file):
-    dir1 = '/home/gennady/python-project-50/files/file1.json'
-    dir2 = '/home/gennady/python-project-50/files/file2.json'
-    # file_1 = json.loads(get_file(dir1))
-    # file_2 = json.loads(get_file(dir2))
+def generate_diff(dir1, dir2):
     file1 = dict(sorted(json.loads(get_file(dir1)).items()))
     file2 = dict(sorted(json.loads(get_file(dir2)).items()))
     all_keys = sorted(set(file1) | set(file2))
-    result = {}
+    result = '{\n'
     for x in all_keys:
         if x not in file2:
-            result[f'{IN_FILE1}{x}'] = file1[x]
+            result += IN_FILE1 + x + ': ' + str(file1[x]) + '\n'
         elif x not in file1:
-            result[f'{IN_FILE2}{x}'] = file2[x]
+            result += IN_FILE2 + x + ': ' + str(file2[x]) + '\n'
         elif x in file1 and file2:
             if file1[x] == file2[x]:
-                result[f'{IN_FILES}{x}'] = file2[x]
+                result += IN_FILES + x + ': ' + str(file2[x]) + '\n'
             else:
-                result[f'{IN_FILE1}{x}'] = file1[x]
-                result[f'{IN_FILE2}{x}'] = file2[x]
+                result += IN_FILE1 + x + ': ' + str(file1[x]) + '\n'
+                result += IN_FILE2 + x + ': ' + str(file2[x]) + '\n'
+    result += '}'
     return result
