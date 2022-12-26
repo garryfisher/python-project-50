8bd82484ef49d7d27b6862285e87ed4c864aacdf
garryfisher <gwinogradow@gmail.com>
2022-12-26 18:25:39 +0300

    FIX tests tree

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -5,12 +5,18 @@ file1_json = path.join(path.dirname("./tests/fixtures/"),
                        "test_file1.json")
 file2_json = path.join(path.dirname("./tests/fixtures/"),
                        "test_file2.json")
+file1_tree_json = path.join(path.dirname("./tests/fixtures/"),
+                            "test_file1_tree.json")
+file2_tree_json = path.join(path.dirname("./tests/fixtures/"),
+                            "test_file2_tree.json")
 file1_yml = path.join(path.dirname("./tests/fixtures/"),
                       "test_file1.yml")
 file2_yml = path.join(path.dirname("./tests/fixtures/"),
                       "test_file2.yml")
 result_type_str = str(path.join(path.dirname("./tests/fixtures/"),
                                 "test_result_json.txt"))
+result_tree_json_str = str(path.join(path.dirname("./tests/fixtures/"),
+                                     "test_result_tree_json.txt"))
 
 
 def test_generate_diff_json():
@@ -21,3 +27,8 @@ def test_generate_diff_json():
 def test_generate_diff_yml():
     with open(result_type_str) as result:
         assert generate_diff(file1_yml, file2_yml) == result.read()
+
+
+def test_generate_diff_tree_json():
+    with open(result_tree_json_str) as result:
+        assert generate_diff(file1_tree_json, file2_tree_json) == result.read()
