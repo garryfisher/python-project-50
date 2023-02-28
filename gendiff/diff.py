4d555a288e5499f6da50cfe9f3de26e92bb9ebdc
garryfisher <gwinogradow@gmail.com>
2023-02-28 20:27:50 +0300

    ADD stylish

--- gendiff/diff.py
+++ gendiff/diff.py
@@ -1,6 +1,3 @@
-from gendiff.stylish import formatter
-
-
 def get_element(key, value, operation):
     return {
         'key': key,
@@ -17,7 +14,7 @@ def get_diff(file1, file2):
         cur_value2 = file2.get(key, 'key_no_found')
         if isinstance(cur_value1, dict) and isinstance(cur_value2, dict):
             diff_list.append(get_element(
-                key, get_diff(cur_value1, cur_value2), 'recursion')
+                key, get_diff(cur_value1, cur_value2), 'children')
             )
         elif cur_value1 == cur_value2:
             diff_list.append(get_element(key, cur_value1, 'no_changed'))
@@ -27,7 +24,8 @@ def get_diff(file1, file2):
             diff_list.append(get_element(key, cur_value2, 'added'))
         else:
             diff_list.append(
-                get_element(key, (cur_value1, cur_value2), 'update')
+                get_element(key, (cur_value1, cur_value2), 'updated')
             )
-    # print(diff_list)
-    return formatter(diff_list)
+    #print(diff_list)
+
+    return diff_list
--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,6 +1,15 @@
 from gendiff.diff import get_diff
-from gendiff.files import get_file
+from gendiff.parser import load_data
+from gendiff.extension.stylish import stylish_format
 
 
-def generate_diff(path_to_file1, path_to_file2):
-    return get_diff(get_file(path_to_file1), get_file(path_to_file2))
+FORMATS = {
+    'stylish': stylish_format,
+}
+
+
+def generate_diff(path_to_file1, path_to_file2, format_type='stylish'):
+    formatter = FORMATS[format_type]
+
+    return formatter(get_diff(load_data(path_to_file1),
+                              load_data(path_to_file2)))
